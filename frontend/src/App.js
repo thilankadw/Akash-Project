5c543085cfc8d04db4f2958e9d37b6722914e126
thilankadw <thilankawijesingham@gmail.com>
2023-12-22 10:03:54 +0530

    react frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+
+import HomePage from './Pages/Home/Home_Page';
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import ContactUs from "./Pages/Contact Us/Contact_Us";
+import OurServices from "./Pages/Our Services/Our_Services";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/services" element={<OurServices />} />
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
